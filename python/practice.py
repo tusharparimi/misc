88617e2272bdd8c60e0fa29263b240a8e28d1e53
Tushar Parimi <93556280+tusharparimi@users.noreply.github.com>
2022-05-18 22:29:11 -0400

    Update practice.py

--- python/practice.py
+++ python/practice.py
@@ -2,3 +2,15 @@
 students = ["Hannah", "Peter", "Luke"]
 new=[s[len(s)::-1] for s in students ]
 print(new)
+
+#Removing nth character from a string
+def nth_char(s,n=None):
+    if n is None:
+        return "no input"
+    if(n>=len(s) or n<0):
+        return "input not valid"
+    if n==len(s)-1:
+        return s[0:n]
+    return s[0:n]+s[n+1:]
+    
+print(nth_char("Laadu",3))
