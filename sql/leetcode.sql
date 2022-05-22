5f33f3d5ccf1f191dfefcdbd5fc0ecc578795d23
Tushar Parimi <93556280+tusharparimi@users.noreply.github.com>
2022-05-21 23:50:38 -0400

    Update leetcode.sql

--- sql/leetcode.sql
+++ sql/leetcode.sql
@@ -28,6 +28,10 @@ SELECT e1.name AS Employee
 FROM Employee e1 
 INNER JOIN Employee e2 ON e1.managerid=e2.id
 WHERE e1.salary>e2.salary;
+
+-- DATEDIFF
+SELECT w1.id FROM Weather w1, Weather w2 
+WHERE DATEDIFF( w1.recordDate, w2.recordDate)=1 AND w1.temperature>w2.temperature;
     
  
  
