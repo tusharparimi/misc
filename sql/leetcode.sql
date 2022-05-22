96e542091f11cf19eae2fc6898d024232acfb3a3
Tushar Parimi <93556280+tusharparimi@users.noreply.github.com>
2022-05-21 22:19:37 -0400

    Update leetcode.sql

--- sql/leetcode.sql
+++ sql/leetcode.sql
@@ -2,3 +2,13 @@
 SELECT MAX(Salary) AS SecondHighestSalary
 FROM Employee
 WHERE Salary < (SELECT MAX(Salary) FROM Employee)
+
+-- Delete duplicate emails
+DELETE FROM Person
+WHERE id IN
+(SELECT id FROM
+(SELECT id,rownum FROM 
+(SELECT id,email,ROW_NUMBER() OVER(PARTITION BY email ORDER BY id) rownum FROM Person) x
+WHERE rownum!=1) 
+ y) 
+ ;
