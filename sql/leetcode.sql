b29ce07c9c27c14b8bd1c9a3cfc25e40120eb04a
Tushar Parimi <93556280+tusharparimi@users.noreply.github.com>
2022-05-23 23:35:43 -0700

    Update leetcode.sql

--- sql/leetcode.sql
+++ sql/leetcode.sql
@@ -3,6 +3,9 @@ SELECT MAX(Salary) AS SecondHighestSalary
 FROM Employee
 WHERE Salary < (SELECT MAX(Salary) FROM Employee)
 
+
+
+
 -- Delete duplicate emails
 -- Using ROW_NUMBER()
 DELETE FROM Person
@@ -13,15 +16,20 @@ WHERE id IN
 WHERE rownum!=1) 
  y) 
  ;
+
  -- Using self join
  DELETE p1 FROM Person p1,
     Person p2
 WHERE
-    p1.Email = p2.Email AND p1.Id > p2.Id;
+    p1.Email = p2.Email AND p1.Id > p2.Id
+;
  -- alternate
   DELETE FROM Person WHERE Id NOT IN 
 (SELECT * FROM(
     SELECT MIN(Id) FROM Person GROUP BY Email) as p);
+
+
+
 
 -- Employee salary greater than manager (self join)
 SELECT e1.name AS Employee 
@@ -29,29 +37,12 @@ FROM Employee e1
 INNER JOIN Employee e2 ON e1.managerid=e2.id
 WHERE e1.salary>e2.salary;
 
--- DATEDIFF
+-- Rising Temperature
 SELECT w1.id FROM Weather w1, Weather w2 
-WHERE DATEDIFF( w1.recordDate, w2.recordDate)=1 AND w1.temperature>w2.temperature;
+WHERE DATEDIFF( w1.recordDate, w2.recordDate)=1 AND w1.temperature>w2.temperature
+;
     
--- Order COunts grouped by month and category
-SELECT SUBSTR(OrderDate,1,7) AS order_month,CategoryName,COUNT(*) AS order_count
-FROM Orders o
-LEFT JOIN OrderDetails od ON o.OrderID=od.OrderID
-LEFT JOIN Products p ON od.ProductID=p.ProductID
-LEFT JOIN Categories c ON p.CategoryID=c.CategoryID
-GROUP BY 1,2
-;
 
--- Top 10 Suppliers wrt order counts
-SELECT SupplierName,COUNT(o.OrderID) AS order_count
-FROM Orders o 
-LEFT JOIN OrderDetails od ON o.OrderID=od.OrderID
-LEFT JOIN Products p ON od.ProductID=p.ProductID
-LEFT JOIN Suppliers s on p.SupplierID=s.SupplierID
-GROUP BY 1
-ORDER BY 2 DESC
-LIMIT 10
-;
 
 -- customer placing largest order
 /*
@@ -71,6 +62,8 @@ ORDER BY COUNT(order_number) DESC
 LIMIT 1
 ;
 
+
+
 -- Sales person not associated with a particular company
 SELECT DISTINCT s.name 
 FROM SalesPerson s
@@ -82,6 +75,9 @@ INNER JOIN Company ON Orders.com_id=Company.com_id
 WHERE Company.name='RED')
 ;
 
+
+
+
 -- product only sold in a date range
 SELECT product_id,product_name 
 FROM Product p
@@ -91,6 +87,9 @@ WHERE product_id NOT IN
  INNER JOIN Product p ON s.product_id=p.product_id
 WHERE sale_date>'2019-03-31' OR sale_date<'2019-01-01'
 );
+
+
+
 
 -- updating a table with case
 UPDATE salary
