1692c7b50cc0b7ff212a134e401e2ce757ddcbdd
Tushar Parimi <93556280+tusharparimi@users.noreply.github.com>
2022-05-31 17:56:26 -0700

    Update practice.py

--- python/practice.py
+++ python/practice.py
@@ -1,4 +1,26 @@
-#String reversal 
+# -------------------------------------------------------------------
+# This implementation of fibonacci series is done by using recursion.
+# Using recursion
+# -------------------------------------------------------------------
+def fib(n,x,y):
+    if n==0:
+        print(0)
+    elif n==1:
+        print(0)
+        print(1)
+    else:
+        print(x)
+        if x+y>=n:
+            print(y)
+            return n
+        return fib(n,y,x+y)
+    
+if __name__ == '__main__':
+    fib(10,0,1)
+
+# -------------------------------------------------------------------
+# String reversal 
+# -------------------------------------------------------------------
 students = ["Hannah", "Peter", "Luke"]
 new=[s[len(s)::-1] for s in students ]
 print(new)
