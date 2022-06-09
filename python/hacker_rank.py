a48c4bb6752abd5d75a2dc61d67ec4e581c875da
Tushar Parimi <93556280+tusharparimi@users.noreply.github.com>
2022-06-09 16:34:55 -0700

    Create hacker_rank.py

---
+++ python/hacker_rank.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+# write your code here
+def avg(*argv):
+    sum=0
+    c=0
+    for arg in argv:
+        sum=sum+arg
+        c=c+1
+    return sum/c
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    
+    nums = list(map(int, input().split()))
+    res = avg(*nums)
+    
+    fptr.write('%.2f' % res + '\n')
+
+    fptr.close()
