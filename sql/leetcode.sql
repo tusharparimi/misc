bce699c340d55a356d8f4f3247f5357ee3ccf15b
Tushar Parimi <93556280+tusharparimi@users.noreply.github.com>
2022-06-16 21:38:54 -0400

    Update leetcode.sql

--- sql/leetcode.sql
+++ sql/leetcode.sql
@@ -211,3 +211,13 @@ FROM Seat
 
 ORDER BY id
 ;
+
+/*--------------------------------------------------------------------------------------
+consequtive free seats
+---------------------------------------------------------------------------------------*/
+
+SELECT DISTINCT c1.seat_id
+FROM Cinema c1 JOIN Cinema c2
+ON ABS(c1.seat_id-c2.seat_id)=1
+AND c1.free=1 AND c2.free=1
+ORDER BY c1.seat_id;
